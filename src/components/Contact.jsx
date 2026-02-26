3b0fb2cddc9a0989bf2f66c1b9a8ed064f0c7989
guialmeida10 <guisardinha09@gmail.com>
2026-02-26 15:17:57 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,17 @@
 import "./Contact.css";
-import { FaEnvelope, FaLinkedin, FaGithub, FaPaperPlane } from 'react-icons/fa';
+import { FaEnvelope, FaLinkedin, FaGithub, FaPaperPlane } from "react-icons/fa";
 
 export default function Contact() {
   return (
     <section id="contact" className="contact">
       <div className="contact-container">
-        
         <div className="contact-info">
-          <h2 className="contact-title">Vamos conversar<span>.</span></h2>
+          <h2 className="contact-title">
+            Vamos conversar<span>.</span>
+          </h2>
           <p className="contact-description">
-            Tem um projeto em mente ou quer apenas trocar uma ideia? 
-            Sinta-se à vontade para me chamar em qualquer rede ou preencher o formulário.
+            Tem um projeto em mente ou quer apenas trocar uma ideia? Sinta-se à
+            vontade para me chamar em qualquer rede ou preencher o formulário.
           </p>
 
           <div className="info-items">
@@ -18,32 +19,54 @@ export default function Contact() {
               <FaEnvelope className="info-icon" />
               <div>
                 <span>E-mail</span>
-                <p>seuemail@exemplo.com</p>
+                <p>guiwork09@exemplo.com</p>
               </div>
             </div>
           </div>
 
           <div className="social-links">
-            <a href="#" className="social-icon"><FaLinkedin /></a>
-            <a href="#" className="social-icon"><FaGithub /></a>
+            <a href="#" className="social-icon">
+              <FaLinkedin />
+            </a>
+            <a href="#" className="social-icon">
+              <FaGithub />
+            </a>
           </div>
         </div>
 
-        <form className="contact-form">
+        <form
+          className="contact-form"
+          action="https://formspree.io/f/maqdvaaz"
+          method="POST"
+        >
+          <input type="hidden" name="_next" value="http://localhost:5173/" />
           <div className="input-group">
-            <input type="text" placeholder="Seu nome" required />
+            {/* O Formspree usa o atributo 'name' para identificar os dados no e-mail */}
+            <input type="text" name="name" placeholder="Seu nome" required />
           </div>
+
           <div className="input-group">
-            <input type="email" placeholder="Seu e-mail" required />
+            <input
+              type="email"
+              name="email"
+              placeholder="Seu e-mail"
+              required
+            />
           </div>
+
           <div className="input-group">
-            <textarea placeholder="Como posso ajudar?" rows="5" required></textarea>
+            <textarea
+              name="message"
+              placeholder="Como posso ajudar?"
+              rows="5"
+              required
+            ></textarea>
           </div>
+
           <button type="submit" className="submit-btn">
             Enviar Mensagem <FaPaperPlane />
           </button>
         </form>
-
       </div>
     </section>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,8 @@ export default function Projects() {
     { id: 1, title: "Projeto TO-DO-LIST", tech: "HTML • CSS • JavaScript", desc: "Lista de tarefas com CRUD completo.", img: "./src/assets/to-do-list.jpeg", github: "https://github.com/guialmeida10/to-do-list", link: "https://guialmeida10.github.io/to-do-list /" },
     { id: 2, title: "Projeto Quiz Futebol", tech: "HTML • CSS • JavaScript", desc: "Quiz de Futebol com perguntas e respostas.", img: "./src/assets/quiz.jpeg", github: "https://github.com/guialmeida10/Quiz-Futebol", link: "https://guialmeida10.github.io/Quiz-Futebol/" },
     { id: 3, title: "Projeto Store-System", tech: "React.js • Node.js • MySQL", desc: "Tela de login e CRUD de produtos.", img: "./src/assets/store-system.jpeg", github: "https://github.com/guialmeida10/store-system", link: "https://guialmeida10.github.io/store-system/" },
+    { id: 4, title: "Projeto GuiLinks", tech: "React.js • Node.js • MySQL", desc: "Árvore de links ", img: "./src/assets/guilinks.jpeg", github: "https://github.com/guialmeida10/GuiLinks", link: "https://guialmeida10.github.io/GuiLinks/" },
+    { id: 5, title: "Projeto Fisio System", tech: "React.js • Node.js • MySQL", desc: "Aplicação de músicas com React.", img: "./src/assets/trybetunes.jpeg", github: "https://github.com/guialmeida10/trybetunes", link: "https://guialmeida10.github.io/trybetunes/" }
   ];
 
   return (
